9807fbe63243397c70cf068be20e31474db082fb
m02xime <maxkb02@gmail.com>
2021-11-01 18:10:16 +0100

    footer:update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,8 +8,8 @@ function Footer() {
         <footer className="footer">
             <div className="icons">
                 <ul className="fo-links">
-                    <li><img src={Linkedin} className="fo-logo" alt="logo" /></li>
-                    <li><img src={Github} className="fo-logo" alt="logo" /></li>
+                    <li><a href="https://github.com/m02xime" target="_blank" rel="noreferrer noopener"><img src={Linkedin} className="fo-logo" alt="logo" /></a></li>
+                    <li><a href="https://www.linkedin.com/in/maxime-kock-bouabid/" target="_blank" rel="noreferrer noopener"><img src={Github} className="fo-logo" alt="logo" /></a></li>
                 </ul>
                 <p className="company-name">
                     Maxime Kock--Bouabid &copy; 2021, ALL Rights Reserved
